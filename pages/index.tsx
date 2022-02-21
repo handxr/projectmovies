c7909d233fd72afa421f069dac02af062775e596
Juan <hander.juan@gmail.com>
2022-02-21 18:03:39 +0100

    yeeoeo

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export async function getServerSideProps(ctx: NextPageContext) {
 
   const request = await fetch(
     `https://api.themoviedb.org/3${
-      requests[genre].url || requests.fetchTrending.url
+      requests[genre]?.url || requests.fetchTrending.url
     }`
   ).then((res) => res.json());
 
